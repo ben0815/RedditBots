380fcd4715f51b70fc4c5f7cc0d94fc4fe79171d
Ben Smith <smithbr95@gmail.com>
2020-02-14 23:34:36 -0800

    Complete 1st implementation of Alert bot

--- Alert/README.md
+++ Alert/README.md
@@ -8,35 +8,45 @@ search existing posts on the subreddit.
 ## Configuration
 
 Configure this bot by editing the `src/configuration.json` file. This JSON file is
-structured as an object containing alerts as name, object pairs.
+structured as an object containing various fields that make up an alert. All fields (shown below) should be filled.
 
 ```
 // configuration.json structure
 
 {
-  "<alert-name>": {
-    "email": "<email-address>",
-    "subreddits": [
-      "<subreddit-1>",
-      "<subreddit-2>",
-             .
-             .
-             .
-      "<subreddit-n>",
-    ],
-    "keywords": [
-      "<keyword-1>",
-      "<keyword-2>",
-            .
-            .
-            .
-      "<keyword-n>",
-    ]
-  },
-  ... // more alerts
+  "name": "<alert-name>",
+  "email": "<gmail-address>",
+  "subreddits": [
+    "<subreddit-1>",
+    "<subreddit-2>",
+           .
+           .
+           .
+      "<subreddit-n>"
+  ],
+  "patterns": [
+    "<pattern-1>",
+    "<pattern-2>",
+          .
+          .
+          .
+    "<pattern-n>"
+  ]
 }
 ```
 
-### Keywords
+where each pattern is a regular expression. This implemetnation uses the Gmail API to insert emails into the inbox of the email address provided and so it must be a Gmail address. Note, no input validation is done on these fields.
 
-TODO: Write about what type of keywords can be used.
+### Setup Gmail
+
+This bot uses the [Gmail API](https://developers.google.com/gmail/api/quickstart/python) to send emails. It will send emails to and from the address configured. Before using this bot you will need to setup Gmail authentication. 
+
+1. Open [this site](https://developers.google.com/gmail/api/quickstart/python) in a browser and complete Step 1. Place the populated credentials.json file in this RedditBots/Alert/src`.
+2. Run `quickstart.py` from within this directory and allow access to your Gmail account
+3. (2) will create a token.pickle file within the RedditBots directory. This file is used for authentication.
+
+### Multiple alerts
+
+Unfortunately, PRAW's Reddit instance is [not thread safe](https://praw.readthedocs.io/en/latest/getting_started/multiple_instances.html#multiple-threads). As a result this bot cannot run multiple alerts at the same time. Each alert would need to run in a separate thread and access a single Reddit instance which is not safe.  
+
+As mentioned in the docs multiple alerts will require multiple processes.
--- Alert/src/alert.py
+++ Alert/src/alert.py
@@ -1,8 +1,23 @@
 #!/usr/local/bin/python3
 
+import base64
+import os
+import pickle
 import praw
 import json
+import re
+import smtplib
+import sys
 
+from apiclient import errors
+
+from email.mime.text import MIMEText
+
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+SCOPES = ['https://www.googleapis.com/auth/gmail.send']
 
 # Initialize reddit object with .ini file.
 reddit = praw.Reddit('Alert')
@@ -11,25 +26,57 @@ def main():
     print("Successfully logged into Reddit with username ", reddit.user.me(),
             ".", sep = '')
 
-    with open('configuration.json') as configuration_file:
+    with open(os.path.join(sys.path[0], 'configuration.json')) as configuration_file:
         configuration = json.load(configuration_file)
 
-    alert = configuration["test-alert"]
-    email = alert["email"]
-    subreddits = alert["subreddits"]
-    keywords = alert["keywords"]
+    service = setup_gmail()
 
-    print(email)
-    print(subreddits)
-    print(keywords)
+    # For some reason reddit.multireddit does not work for me.
+    multireddit = '+'.join(configuration["subreddits"])
 
-    # Create multireddit of subreddits
-    # stream new submissions
-    # Search for keywords
+    print(multireddit)
 
-    # Continuously search for phrases in comments of given subreddit(s).
-    while(True):
-        pass
+    re_objects = [re.compile(pattern) for pattern in configuration["patterns"]]
+    for submission in reddit.subreddit(multireddit).stream.submissions(skip_existing=True):
+        for reo in re_objects:
+            if re.search(reo, submission.title + submission.selftext) is not None:
+                send_email(service, configuration["email"], create_message(configuration["email"], 'Alert - ' + configuration["name"], submission.shortlink))
+
+def setup_gmail():
+    creds = None
+
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                os.path.join(sys.path[0], 'credentials.json'), SCOPES)
+            creds = flow.run_local_server(port=0)
+
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    return build('gmail', 'v1', credentials=creds)
+
+def create_message(address, subject, body):
+    message = MIMEText(body)
+    message['to'] = address
+    message['from'] = address
+    message['subject'] = subject 
+    return {'raw': base64.urlsafe_b64encode(message.as_bytes()).decode()}
+
+def send_email(service, address, message):
+    try:
+        print('Sending email: ', message)
+        service.users().messages().send(userId=address, body=message).execute()
+    except errors.HttpError as error:
+        print('An error occurred: ', error)
 
 if __name__ == '__main__':
     main()
